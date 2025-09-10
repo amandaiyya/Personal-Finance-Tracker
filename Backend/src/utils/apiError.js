b5da395015e1b04f0acacb521b472174ffa79882
amansain11 <sainaman1819@gmail.com>
2025-09-10 18:50:45 +0530

    Added some utility classes

---
+++ Backend/src/utils/apiError.js
@@ -0,0 +1,15 @@
+class apiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = []
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.message = message
+        this.success = false
+        this.errors = errors
+    }
+};
+
+export default apiError;
